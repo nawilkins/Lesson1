e1bfc787e0b7dc606f65a04870ef2c101a9544d5
Newlin Wilkins <nwilkins89@gmail.com>
2014-04-12 12:01:32 -0500

    lesson 1 quiz complete

---
+++ lesson_1_quiz.rb
@@ -0,0 +1,27 @@
+#1.)
+puts "a is equal to 1"
+
+#2.)
+puts "Array is a numerically indexed collection of objects, whereas Hash is a value-indexed collection of objects, where the value can be pretty much anything; key-value pairs."
+
+#3.)
+puts "First one: 1, 2, 3; values of arr = [1, 2, 3, 3]"
+puts "Second one: 1, 2, 3; values of arr = [1, 2, 3]"
+
+#4.)
+puts "Map invokes the block for each element of the array, returning a NEW array with the modified elements."
+puts "Select returns a new array containing all the elements of the original that satisfy a given block"
+puts "Map usage, when you want to perform an action on each element of the array and get the returned values"
+puts "Select usage, when you want to find the elements of the array that satisfy a condition, but aren't performing a transmutation on them (yet)"
+
+#5.)
+puts "Instead of the {a: 1} method of defining the hash keys, you would use the default {\"string\" => value} method for defining the keys."
+
+#6.)
+puts "no way!"
+
+#7.)
+puts "x = 4"
+
+#8.)
+puts "x = 7"
